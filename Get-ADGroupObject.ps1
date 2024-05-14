b642b91d47e358354f7b63d144a53b2b3ba086e8
Renato D. Mejilla <rene.midland1@gmail.com>
2024-05-14 16:33:48 +0800

    Add files via upload

---
+++ Get-ADGroupObject.ps1
@@ -0,0 +1,44 @@
+﻿param ($CSVPath)
+Import-Csv $CSVPath | % {
+
+    #$Filter = "'" + 'CN -eq ' + '"' + $_.ADObject + '"' + "'"
+
+    Write-Host "Getting information for $($_.ADObject)"
+    $Filter = 'CN -eq ' + '"' + $_.ADObject + '"' + ' -or SamAccountName -eq ' + '"' + $_.ADObject + '"'
+
+    $obj = Get-ADObject -Filter $Filter -SearchBase 'DC=PMINTL,DC=NET' -Server PMICHLAUGC34
+
+    if ($obj.ObjectClass -eq "group") {
+        $i = Get-ADGroup -Identity $obj.DistinguishedName -Properties ManagedBy -Server PMICHLAUGC34
+        try {
+            $m = (Get-ADUser $i.ManagedBy -Server PMICHLAUGC34).Name
+        }
+        catch {
+            $m =""
+        }
+
+        $i.Name
+        New-Object psobject -Property @{
+            Name = $i.Name;
+            SamAccountName = $i.SamAccountName;
+            'Supervisor/Owner' = $m
+        } | Export-Csv "C:\Temp\SharedFolderUsers1.Csv" -NoTypeInformation -Append
+    }
+
+    if ($obj.ObjectClass -eq "user") {
+        $i = Get-ADUser -Identity $obj.DistinguishedName -Properties Manager -Server PMICHLAUGC34
+        try {
+            $m = (Get-ADUser $i.Manager -Server PMICHLAUGC34).Name 
+        }
+        catch {
+            $m = ""
+        }
+
+        $i.Name
+        New-Object psobject -Property @{
+            Name = $i.Name;
+            SamAccountName = $i.SamAccountName;
+            'Supervisor/Owner' = $m
+        } | Export-Csv "C:\Temp\SharedFolderUsers1.Csv" -NoTypeInformation -Append
+    }
+}
