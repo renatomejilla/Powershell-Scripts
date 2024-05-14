6a7e4c8d40526a56c2e8d45d010118b5ba914654
Renato D. Mejilla <rene.midland1@gmail.com>
2024-05-14 16:35:31 +0800

    Add files via upload

---
+++ Get-OnPremServers.ps1
@@ -0,0 +1,5 @@
+﻿$Date=Get-Date -Format MMM-dd-yyy
+Get-ADComputer -properties OperatingSystem,CanonicalName -Filter `
+    '(OperatingSystem -like "Windows Server*")' | ? {($_.CanonicalName -NotMatch "INT-SDI") -And ($_.CanonicalName -NotMatch "INT-Cloud")} `
+    | Select Name,CanonicalName,OperatingSystem | Export-Csv "C:\Temp\WinServers_$Date.csv" -NoTypeInformation
+
