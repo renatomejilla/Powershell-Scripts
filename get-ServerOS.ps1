99a2d776d398460656fef4c6447588bea6c47751
Renato D. Mejilla <rene.midland1@gmail.com>
2024-05-14 16:26:36 +0800

    Add files via upload

---
+++ get-ServerOS.ps1
@@ -0,0 +1,19 @@
+﻿param ($SFile, $OFile)
+
+$SRVs = get-content $SFile
+
+$a = @()
+
+foreach ($srv in $SRVs) {
+
+    try {
+        Write-Host "Checking $srv"
+        $tmp = get-adcomputer $srv -Properties * | Select Name,OperatingSystem,LastLogonDate,Created
+        $a += New-Object PSObject -Property @{MachineName = $tmp.name; OperatingSystem = $tmp.OperatingSystem; LastLogonDate = $tmp.LastLogonDate; DateCreated=$tmp.Created; Comment=""}
+    }
+    catch {
+        $a += New-Object PSObject -Property @{MachineName = $srv; OperatingSystem = ""; LastLogonDate = ""; DateCreated=""; Comment = "Not found in AD"}
+    }
+}
+
+$a | Export-Csv -Path $OFile -force -NoTypeInformation
